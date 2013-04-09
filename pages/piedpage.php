a97aae44d20ac74c08c36726a440194722f90a71
Your Name <you@example.com>
2013-04-09 17:09:57 +0200

    modified:   pages/piedpage.php

--- pages/piedpage.php
+++ pages/piedpage.php
@@ -11,7 +11,6 @@
 				<td width="669">
 					H-HD. &copy; 2009-2013 - Siren :  - All rights reserved - Panel v1.2 BETA panel by H-HD
 					<br /><br />
-					<img width=85 height=16 border=0 alt='website uptime' src="http://btn.siteuptime.com/genbutton.php?u=106526&m=94020&c=blue&p=total"></a><noscript><a href='http://www.siteuptime.com/'>website monitoring</a></noscript>
 				</td>
 			</tr>
 		</table>
